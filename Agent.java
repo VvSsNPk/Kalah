41883e639d3ecebf9a74f3c7abe9082b7bf6f0d1
PraveenRoyal <30446334+PraveenRoyal@users.noreply.github.com>
2024-01-10 16:10:17 +0100

    updated minmax for normal purpose

--- Agent.java
+++ Agent.java
@@ -29,91 +29,34 @@ class Agent extends info.kwarc.kalah.Agent {
 
     @Override
     public void search(KalahState ks) throws IOException {
-        int bestMove = 0;
-        int bestState = Integer.MIN_VALUE;
-        int state;
-        int depth = 1;
-        MINMAX south;
-        if(ks.getSideToMove() == KalahState.Player.SOUTH){
-            south = MINMAX.MAX;
-        }else{
-            south = MINMAX.MIN;
-        }
-        for(Integer a : ks.getMoves()){
-            while (!shouldStop()) {
-                if (ks.isLegalMove(a)) {
-                    KalahState copy = new KalahState(ks);
-                    copy.doMove(a);
-                    state = minmax(copy, depth, Integer.MIN_VALUE, Integer.MAX_VALUE, south);
-                    if (state > bestState) {
-                        bestState = state;
-                        bestMove = a;
-                    }
-                }
-            }
-            depth++;
-        }
-        submitMove(bestMove);
+       submitMove(ks.randomLegalMove());
     }
 
-    private int minmax(KalahState a, int depth, int alpha, int beta, MINMAX minmax) {
-        int bestState = 0;
-        int state;
-        if(depth <= 0) return  evaluation(a,minmax);
-        //bestState = (a.getSideToMove() == KalahState.Player.SOUTH) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-        for(int i = 0; i<a.getBoardSize();i++){
-            if(a.isLegalMove(i)){
-                KalahState copy = new KalahState(a);
-                copy.doMove(i);
-                if (minmax == MINMAX.MIN){
-                    bestState = -1000000000;
-                    state = minmax(copy,--depth,alpha,beta, minmax);
-                    bestState = Math.min(bestState,state);
-                    alpha = Math.min(alpha,bestState);
-                }
-                else {
-                    bestState = 1000000000;
-                    state = minmax(copy, --depth,alpha,beta, minmax);
-                    bestState = Math.max(bestState, state);
-                    beta = Math.max(beta, bestState);
-                }
-                if(beta <= alpha){
-                    break;
-                }
-            }
-        }
+    private int minmax(KalahState a, int depth, int alpha, int beta) {
 
-        return bestState;
+        return 0;
+
     }
 
-    public int evaluation(KalahState a,MINMAX minmax){
-        int eval = a.getHouseSumSouth() -a.getHouseSumNorth();
-        if(minmax == MINMAX.MAX){
-            if(eval > 0){
-                return eval;
-            }else{
-                return -eval;
-            }
-        }else{
-            if(eval < 0){
-                return eval;
-            }else{
-                return  -eval;
-            }
-        }
+    private void maxvalue(KalahState a, int depth, int alpha, int beta){
+
+    }
+
+    private void minvalue(KalahState a, int depth, int alpha, int beta){
+
+    }
+
+    public int evaluation(KalahState a){
+        return 0;
     }
 
 
 
     public static void main(String[] args) throws IOException {
-        new Agent().run();
+        //new Agent().run();
 //        KalahState ks = new KalahState(8,8);
 //        System.out.println(ks.getSideToMove());
 //        ks.doMove(1);
     }
 
-    public enum MINMAX{
-        MIN,
-        MAX,
-    }
 }
